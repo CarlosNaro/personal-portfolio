fb66a1e59194f20629ae688d374de471c1911336
CarlosNaro <elmaximolider75@gmail.com>
2024-10-07 10:56:02 -0500

    README

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
 
+# Portafolio de Desarrollador
+
+Este proyecto es un portafolio personal desarrollado con Nuxt 3, TypeScript y Vue.js. Su objetivo es mostrar mis
+habilidades, proyectos y experiencia como desarrollador de software. Incluye características como:
+
+- **Modo oscuro**: Soporte para cambiar entre modo claro y oscuro.
+- **Componentes reutilizables**: Uso de componentes Vue para una estructura modular y mantenible.
+- **Estilos con Tailwind CSS**: Utilización de Tailwind CSS para un diseño rápido y eficiente.
+- **Transiciones y animaciones**: Efectos de transición y animación para una mejor experiencia de usuario.
+- **Sección de proyectos**: Muestra de los proyectos más destacados en los que he trabajado.
+- **Sección de contacto**: Formulario para que los visitantes puedan ponerse en contacto conmigo.
+
 ## Setup
 
 Make sure to install the dependencies:
--- components/Home/index.vue
+++ components/Home/index.vue
@@ -6,17 +6,22 @@ const isDarkMode = computed(() => globalStore.getIsDarkMode());
 </script>
 
 <template>
-  <div class="flex gap-2">
+  <div class="noSelect flex md:p-2 gap-2 md:gap-28 lg:gap-2 lg:mx-32 2xl:mx-48">
     <div class="flex-1 flex-col">
-      <h1 class="text-2xl font-semibold">¡Hola!</h1>
-      <p class="text-justify">
+      <h1 class="text-2xl md:text-[2rem] font-semibold">¡Hola!</h1>
+      <p class="text-justify md:text-[2rem]">
         Me llamo <b>Alonso</b>, soy un desarrollador de software apasionado por crear soluciones tecnológicas eficientes
         y escalables.
       </p>
       <button class="button-default"><span class="ns-button__content">Download CV</span></button>
     </div>
-    <div class="card flex justify-center items-center rounded-b-full w-44">
-      <img :class="isDarkMode ? 'bg-Content' : 'bg-light'" class="rounded-b-full" :src="profilePng" alt="" />
+    <div class="flex justify-center items-center rounded-b-full">
+      <img
+        :class="isDarkMode ? 'bg-Content' : 'bg-light'"
+        class="card rounded-b-full w-40 md:w-52"
+        :src="profilePng"
+        alt=""
+      />
     </div>
   </div>
 </template>
@@ -29,5 +34,17 @@ const isDarkMode = computed(() => globalStore.getIsDarkMode());
   &:hover {
     box-shadow: 0 10px 20px -10px rgba(var(--ns-warn), 1) !important;
   }
+}
+
+.noSelect {
+  -webkit-user-select: none; /* Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
+}
+
+.description {
+  /*padding: 1rem;*/
+  /* font-size: 2rem;*/
 }
 </style>
