e30d3f2a87e116556d0d0261bf25dba716e9e5a6
CarlosNaro <elmaximolider75@gmail.com>
2025-03-25 22:00:57 -0500

    mejoras del diseño de la imagen 2

--- components/About/index.vue
+++ components/About/index.vue
@@ -7,8 +7,8 @@ import foto2 from '../../assets/img/foto_2.png';
 <template>
   <LayoutBase>
     <component-title title="Sobre mi" />
-    <div class="flex">
-      <div class="flex flex-col mt-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 content-between mt-5">
+      <div class="flex m-auto flex-col">
         <span class="mb-2">
           Mi nombre es Alonso Naro Saldaña. Soy ingeniero de sistemas e informática, graduado de la Universidad Nacional
           de la Amazonía Peruana. Soy de nacionalidad peruana y natural de la ciudad de Iquitos, en la región de Loreto.
@@ -20,8 +20,16 @@ import foto2 from '../../assets/img/foto_2.png';
           intuitivo y funcionalidades robustas.
         </span>
       </div>
-      <div class="relative flex items-center justify-center bgColor rounded-full w-full h-72 px-6 shadow-xl">
-        <img class="absolute -top-24 fade-out rounded-br-[145px] mirror" :src="foto2" alt="foto2" />
+      <div class="flex w-full items-center justify-center md:justify-end">
+        <div
+          class="mirror relative mt-5 flex items-center justify-center bgColor rounded-full w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[270px] lg:h-[270px] xl:w-[300px] xl:h-[300px] shadow-xl"
+        >
+          <img
+            class="absolute -top-20 md:-top-32 right-0 md:right-1 rounded-r-[100px] rounded-br-[80px] md:rounded-br-[95px] fade-out"
+            :src="foto2"
+            alt="foto2"
+          />
+        </div>
       </div>
     </div>
   </LayoutBase>
@@ -30,8 +38,8 @@ import foto2 from '../../assets/img/foto_2.png';
 <style scoped>
 .bgColor {
   /*--ns-warn: 255, 186, 0;*/
-  /*background: linear-gradient(45deg, rgb(255, 186, 0), rgb(0, 0, 0));*/
-  background: rgb(var(--ns-warn));
+  background: linear-gradient(45deg, rgb(255, 186, 0), rgb(0, 0, 0));
+  /*background: rgb(var(--ns-warn));*/
 }
 
 .fade-out {
